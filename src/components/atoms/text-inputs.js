ddc68ecdeeed6273c797ca9377bb02dbf4101b95
rorymcdonnell <rorymcdonnell95@gmail.com>
2021-07-22 13:01:59 +0200

    react-native-setup-and-testing

---
+++ src/components/atoms/text-inputs.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, TextInput, Button } from "react-native";
+
+const UsernameInput = () => {
+  const [text, onChangeText] = React.useState("");
+  const [username, setUsername] = React.useState("");
+
+  console.log(username);
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Username"
+        keyboardType="default"
+      />
+      <Button
+        style={styles.button}
+        onPress={() => setUsername(text)}
+        title="Confirm Username"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+  },
+  button: {
+    color: "#841584",
+  },
+});
+
+export default UsernameInput;
